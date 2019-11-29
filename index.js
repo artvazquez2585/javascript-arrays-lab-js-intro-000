250ad3dc63cb168f06202232a12dd994f977c34e
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-29 18:44:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 var kittens = ["Milo","Otis","Garfield"] //define your array here
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  kittens = [...kittens,name]
+  return kittens
+}
+function destructivelyPrependKitten(name){
+  kittens.shift(name)
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+function desctructivelyRemoveFirstKitten(){
+  kittens.unshift()
+  return kittens
+}
